280fbfec8d3aee535d586712bf8483047fa5f52b
Eddy <63247362+Maestro2496@users.noreply.github.com>
2025-04-02 18:47:29 +0100

    Folder structure setup

---
+++ helpers/main.py
@@ -0,0 +1,8 @@
+# Retrieve env variables from secrets manager or parameter store
+def get_env_variables():
+    pass
+
+
+# Create an env file with the retrieved variables
+def create_env_file():
+    pass
